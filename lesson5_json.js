7f59679a7543339c79a3f67ebe33283b8961b742
Dmytro Kosenko <dmitriy.kosenko1@gmail.com>
2023-08-31 19:50:24 +0300

    added_changes_with_new_lesson

--- lesson5_json.js
+++ lesson5_json.js
@@ -1,3 +1,5 @@
+const values = require("./values.json");
+
 let obj1 = { firstName: "dmytro", lastName: "kosenko", age: 32 };
 console.log(obj1);
 
@@ -11,3 +13,5 @@ console.log(jsonParse.firstName);
 
 jsonParse.city = "Kyiv";
 console.log(jsonParse);
+
+console.log(values);
