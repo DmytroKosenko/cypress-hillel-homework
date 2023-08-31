65af72055397598a940291062712dc94c0fac870
Dmytro Kosenko <dmitriy.kosenko1@gmail.com>
2023-08-31 19:39:18 +0300

    added_work_with_json_file

---
+++ lesson5_json.js
@@ -0,0 +1,13 @@
+let obj1 = { firstName: "dmytro", lastName: "kosenko", age: 32 };
+console.log(obj1);
+
+let jsonObj = JSON.stringify(obj1);
+console.log(jsonObj);
+
+let obj2 = '{"firstName":"dmytro","lastName":"kosenko","age":32}';
+console.log(obj2);
+let jsonParse = JSON.parse(obj2);
+console.log(jsonParse.firstName);
+
+jsonParse.city = "Kyiv";
+console.log(jsonParse);
