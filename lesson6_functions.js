600bf95deeee12e1de49e3b02cc34346a41e9207
Dmytro Kosenko <dmitriy.kosenko1@gmail.com>
2023-09-01 14:16:25 +0300

    info_from_lessons_5_and_6

---
+++ lesson6_functions.js
@@ -0,0 +1,25 @@
+// function clickButton() {
+//     console.log("Click the element');
+//     frameworkPlaywright.clickButton();
+// }
+
+// function clickButton(selectors){
+//     console.log("Click the element');
+//     frameworkPlaywright.clickButton(selectors);
+// }
+
+// function clickButton(selectors, text = "Hello") {
+//   let ownVariable2 = "local value";
+//   return ownVariable2;
+// }
+
+// let valueFromFunction = clickButton();
+// console.log(valueFromFunction);
+
+//function expression
+
+let getTheValue = function () {
+  console.log("fetching the data from UI");
+};
+
+console.log(getTheValue());
