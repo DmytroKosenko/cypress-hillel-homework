f8b1615c1511bd66c4a3f335c8430dde141f6724
Dmytro Kosenko <dmitriy.kosenko1@gmail.com>
2023-09-28 21:10:17 +0300

    tests_on_lesson_14

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("loginAndSetCookie", (url, cookieN, cookieV) => {
+  cy.visit(url);
+  cy.setCookie(cookieN, cookieV);
+});
