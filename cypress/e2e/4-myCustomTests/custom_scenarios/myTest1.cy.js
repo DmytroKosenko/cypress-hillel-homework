7f740a7d9844a771fc5d4bd10d1bddfd0f74ef32
Dmytro Kosenko <dmitriy.kosenko1@gmail.com>
2023-10-03 21:14:25 +0300

    lesson_15

--- cypress/e2e/4-myCustomTests/custom_scenarios/myTest1.cy.js
+++ cypress/e2e/4-myCustomTests/custom_scenarios/myTest1.cy.js
@@ -8,7 +8,8 @@ describe("Testing Guru Website", () => {
   });
 
   it("Open the website", () => {
-    cy.get("h1.thick-heading").should("be.visible");
+    cy.get("h1.thick-heading", { timeout: 6000 }).should("be.visible"); //add timeout to concrete element to wait him
+    cy.wait(4000);
   });
 
   it.skip("check search and validate results", () => {
@@ -24,18 +25,50 @@ describe("Testing Guru Website", () => {
     cy.get("div#resInfo-0").should("not.be.empty").and("contain", "About");
   });
 
-  it("Click on softwear testing variant from drop-down list", () => {
+  it.skip("Click on softwear testing variant from drop-down list", () => {
     cy.get("#primary-menu span.nav-drop-title-wrap").eq(0).click();
     // cy.get('a[aria-current="page"] + ul.sub-menu.clicked').should('be.visible') doesn't work
     cy.get("ul.sub-menu.clicked li")
       .eq(7)
       .find("a")
       .should("have.attr", "href", "/software-testing.html")
-      .and("have.text", "Software Testing")
+      .and("have.text", "Software Testing", { timeout: 5000 }) //add timeout to concrete element to wait him
       .click();
     cy.get("div.code-block-1 + h2")
       .should("contain", "Software Testing Training Summary")
       .and("have.css", "margin-top", "0px");
     cy.getAllCookies("").should("exist");
+
+    // cy.get("p strong").then(($textFundamentalsElement) => {
+    //   $textFundamentalsElement.text();
+    //   cy.wait($textFundamentalsElement);
+    // });
+    // cy.intercept("GET", "/software-testing").as("@pageForSoftwareTesting");
+    // cy.wait("@pageForSoftwareTesting");
+
+    cy.get("p strong")
+      .eq(1)
+      .then(($textFundamentalsElement) => {
+        cy.wrap($textFundamentalsElement).should(
+          "contain.text",
+          "Testing Fundamentals"
+        );
+        debugger;
+      });
+  });
+
+  //types of debugger and logger
+  it.skip("Open the software from main page", () => {
+    cy.get("a[title='Software Testing']").then(($linkMainPage) => {
+      debugger;
+    });
+  });
+  it("Open the software from main page", () => {
+    //cy.get("a[title='Software Testing']").debug();
+    cy.get("a[title='Software Testing']");
+    cy.log("Open the Software Testing section");
+    cy.get("h2").pause();
+    cy.task("LOG_INFO", "Another log info message");
+    cy.get("table.table").eq(0).pause();
   });
 });
